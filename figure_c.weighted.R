97a755161e6d464bf206b67c46e7bd900f6d87e2
Duccio Rocchini <ducciorocchini@gmail.com>
2026-01-20 14:14:41 +0100

    Update figure_c.weighted.R

--- figure_c.weighted.R
+++ figure_c.weighted.R
@@ -4,7 +4,25 @@ library(patchwork)   # for combining ggplots
 library(cellulaR)
 
 # Run the weighted simulation
-weighted_sim <- c.weighted()
+weighted_sim <- 
+c.weighted(
+  num_iterations = 100,
+  n_rows = 100,
+  n_cols = 100,
+  frequency = 0.05,
+  octaves = 5,
+  base_growth = 0.1,
+  death_prob = 0.02,
+  init_n = 200,
+  alpha_elev = 1,
+  alpha_slope = 1,
+  neighbor_threshold = 1,
+  kernel = "moore",
+  plot_n = 9,
+  direction=-1,
+  return_model = TRUE,
+  seed = 42
+) 
 
 # Extract individual plots
 p1 <- weighted_sim$terrain_plot
